336137c819bb5b5796c4d978ad46c6e1cf41be19
LuiMar11 <64877820+LuiMar11@users.noreply.github.com>
2023-02-08 16:00:09 -0500

    icon y css

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
 
-const apiKey = "4d8fb5b93d4af21d66a2948710284366";
+const apiKey = "d56b760b57258461022fbf33b9142b42";
 
-form.addEventListener("submit", e => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   let inputVal = input.value;
 
@@ -14,9 +14,9 @@ form.addEventListener("submit", e => {
   const listItemsArray = Array.from(listItems);
 
   if (listItemsArray.length > 0) {
-    const filteredArray = listItemsArray.filter(el => {
+    const filteredArray = listItemsArray.filter((el) => {
       let content = "";
-   
+
       if (inputVal.includes(",")) {
         if (inputVal.split(",")[1].length > 2) {
           inputVal = inputVal.split(",")[0];
@@ -33,7 +33,7 @@ form.addEventListener("submit", e => {
     });
 
     if (filteredArray.length > 0) {
-      msg.textContent = `You already know the weather for ${
+      msg.textContent = `Ya se esta mostrando la ciudad ${
         filteredArray[0].querySelector(".city-name span").textContent
       } `;
       form.reset();
@@ -42,42 +42,37 @@ form.addEventListener("submit", e => {
     }
   }
 
-  //ajax here
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric&lang=es`;
 
   fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const { main, name, sys, weather } = data;
-      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-        weather[0]["icon"]
-      }.svg`;
-
+    .then((response) => response.json())
+    .then((data) => {
+      const { main, name, sys, weather , wind} = data;
+      const icon = ` http://openweathermap.org/img/wn/${weather[0].icon}.png`;
       const li = document.createElement("li");
+
       li.classList.add("city");
-      const markup = `
+      const card = `
         <h2 class="city-name" data-name="${name},${sys.country}">
           <span>${name}</span>
           <sup>${sys.country}</sup>
         </h2>
         <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
         <figure>
-          <img class="city-icon" src="${icon}" alt="${
-        weather[0]["description"]
-      }">
+          <img class="city-icon" src="${icon}" alt="">
           <figcaption>${weather[0]["description"]}</figcaption>
         </figure>
+        <div class = "city-humidity"><b>Humedad</b> ${main.humidity}%</div>
+        <div class = "city-humidity"><b>Velocidad viento </b>${wind.speed} m/s</div>
       `;
-      li.innerHTML = markup;
+      li.innerHTML = card;
       list.appendChild(li);
     })
     .catch(() => {
-      msg.textContent = "Please search for a valid city";
+      msg.textContent = "Busque una ciudad válida";
     });
 
   msg.textContent = "";
   form.reset();
   input.focus();
 });
-
-
